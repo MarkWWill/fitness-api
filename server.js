2ccf531c4795bef3b263cff3ef73e59bf6d6c840
MarkWWill <thewarriorswill42@gmail.com>
2026-03-18 18:35:06 -0700

    Update server.js

--- server.js
+++ server.js
@@ -61,8 +61,6 @@ app.post("/users", async (req, res) => {
   }
 });
 
-const PORT = process.env.PORT || 3000;
-
 app.listen(PORT, "0.0.0.0", () => {
   console.log(`Server running on port ${PORT}`);
 });
