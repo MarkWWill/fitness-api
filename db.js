c8c1a91e2879b51d93bdc3d88813822abd0909d4
MarkWWill <thewarriorswill42@gmail.com>
2026-03-18 18:31:42 -0700

    Update db.js

--- db.js
+++ db.js
@@ -6,7 +6,8 @@ dotenv.config();
 const { Pool } = pg;
 
 export const pool = new Pool({
-  connectionString: process.env.DATABASE_URL
+  connectionString: process.env.DATABASE_URL,
+  ssl: { rejectUnauthorized: false }
 });
 
 export async function query(text, params = []) {
